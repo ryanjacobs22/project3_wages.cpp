0389a969a5520c95cad81746430041547b9b36dd
ryanjacobs22 <43647117+ryanjacobs22@users.noreply.github.com>
2021-11-03 09:45:06 -0400

    Update project3_wages.cpp

--- project3_wages.cpp
+++ project3_wages.cpp
@@ -13,18 +13,16 @@ using namespace std;
 int main()
 {
     // displays wages of five students
-    
+
     cout << setiosflags(ios::right | ios::fixed | ios::showpoint);
     cout << "Employee" << setw(2) << "  " << setw(10) << "Hourly Wage" << endl;
     cout << endl;
     cout << 1 << setw(10) << "  " << setw(3) << "$ " << setprecision(2) << 12.65 << endl;
     cout << 2 << setw(10) << "  " << setw(3) << "$ " << setprecision(2) << 15.10 << endl;
     cout << 3 << setw(10) << "  " << setw(3) << "$ " << setprecision(2) << 13.89 << endl;
-    cout << 4 << setw(10) << "  " << setw(4) << "$ " << setprecision(2) << 9.00 << endl;
-    cout << 5 << setw(10) << "  " << setw(4) << "$ " << setprecision(2) << 9.50 << endl;
-    
+    cout << 4 << setw(10) << "  " << setw(3) << "$ " << setw(5) << setprecision(2) << 9.00 << endl;
+    cout << 5 << setw(10) << "  " << setw(3) << "$ " << setw(5) << setprecision(2) << 9.50 << endl;
+
 
     return 0;
 }
-
-
